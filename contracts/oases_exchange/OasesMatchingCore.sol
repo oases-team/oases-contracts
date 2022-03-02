f2f58b683dd061ff25ff32318d7aa525cbf8ed96
MichaelWang <michael_wangguanyi@163.com>
2022-03-02 11:51:27 +0800

    function matchAssetTypesFromOrders

--- contracts/oases_exchange/OasesMatchingCore.sol
+++ contracts/oases_exchange/OasesMatchingCore.sol
@@ -8,6 +8,7 @@ import "./Cashier.sol";
 import "./OrderVerifier.sol";
 import "./libraries/TransferHelperLibrary.sol";
 import "./libraries/OrderLibrary.sol";
+import "./libraries/OrderDataParsingLibrary.sol";
 import "../common_libraries/AssetLibrary.sol";
 
 abstract contract OasesMatchingCore is AssetTypeMatcher, Cashier, OrderVerifier {
@@ -37,8 +38,79 @@ abstract contract OasesMatchingCore is AssetTypeMatcher, Cashier, OrderVerifier 
         );
     }
 
+    function matchOrders(
+        OrderLibrary.Order memory leftOrder,
+        OrderLibrary.Order memory rightOrder,
+        bytes calldata leftSignature,
+        bytes calldata rightSignature
+    )
+    external
+    payable
+    {
+        validateOrder(leftOrder, leftSignature);
+        validateOrder(rightOrder, rightSignature);
+        if (leftOrder.taker != address(0)) {
+            require(rightOrder.maker == leftOrder.taker, "unmatched taker of left order");
+        }
+        if (rightOrder.taker != address(0)) {
+            require(rightOrder.taker == leftOrder.maker, "unmatched taker of right order");
+        }
+
+        trade(leftOrder, rightOrder);
+    }
+
+    function trade(OrderLibrary.Order memory leftOrder, OrderLibrary.Order memory rightOrder) internal {
+        (
+        AssetLibrary.AssetType memory matchedMakeAssetType,
+        AssetLibrary.AssetType memory matchedTakeAssetType
+        ) = matchAssetTypesFromOrders(leftOrder, rightOrder);
+
+        bytes32 leftOrderHashKey = OrderLibrary.getHashKey(leftOrder);
+        bytes32 rightOrderHashKey = OrderLibrary.getHashKey(rightOrder);
+
+        OrderDataLibrary.Data memory leftOrderData = OrderDataParsingLibrary.parse(leftOrder);
+        OrderDataLibrary.Data memory rightOrderData = OrderDataParsingLibrary.parse(rightOrder);
+    }
+
+    function matchAssetTypesFromOrders(
+        OrderLibrary.Order memory leftOrder,
+        OrderLibrary.Order memory rightOrder
+    )
+    internal
+    view
+    returns
+    (AssetLibrary.AssetType memory matchedMakeAssetType, AssetLibrary.AssetType memory matchedTakeAssetType)
+    {
+        matchedMakeAssetType = matchAssetTypes(leftOrder.makeAsset.assetType, rightOrder.takeAsset.assetType);
+        require(matchedMakeAssetType.assetClass != 0, "bad match of make asset");
+        matchedTakeAssetType = matchAssetTypes(leftOrder.takeAsset.assetType, rightOrder.makeAsset.assetType);
+        require(matchedTakeAssetType.assetClass != 0, "bad match of take asset");
+    }
+
     // get filled record of each order by its order key hash
     function getFilledRecords(bytes32 orderKeyHash) public view returns (uint256){
         return filledRecords[orderKeyHash];
     }
+
+    function validateOrder(OrderLibrary.Order memory order, bytes memory signature) internal view {
+        OrderLibrary.checkTimeValidity(order);
+        verifyOrder(order, signature);
+    }
+
+    function getOrderFilledRecord(
+        OrderLibrary.Order memory order,
+        bytes32 orderHashKey
+    )
+    internal
+    view
+    returns
+    (uint256){
+        if (order.salt == 0) {
+            return 0;
+        } else {
+            return filledRecords[orderHashKey];
+        }
+    }
+
+    uint256[49] private __gap;
 }
