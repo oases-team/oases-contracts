9cff86d382b1d0f36604828ec35d2365810ae4fa
MichaelWang <michael_wangguanyi@163.com>
2022-03-09 19:13:45 +0800

    test OrderLibrary.checkTimeValidity() && OrderLibrary.calculateRemainingValuesInOrder()

--- test/test_files/OrderLibrary-test.js
+++ test/test_files/OrderLibrary-test.js
@@ -21,41 +21,118 @@ contract("test OrderLibrary.sol", accounts => {
 
     before(async () => {
         mockOrderLibrary = await MockOrderLibrary.new()
-    });
+    })
 
     describe("test OrderLibrary.calculateRemainingValuesInOrder()", () => {
         it("should calculate remaining when fill is 0", async () => {
             let mockOrder = ZERO_ORDER
-            mockOrder.makeAsset.value = 100
-            mockOrder.takeAsset.value = 200
+            mockOrder.makeAsset.value = 1000
+            mockOrder.takeAsset.value = 2000
             const res = await mockOrderLibrary.calculateRemainingValuesInOrder(mockOrder, 0, false)
-            assert.equal(res[0], 100)
-            assert.equal(res[1], 200)
-        });
-
-        it("should revert when fill > asset's values", async () => {
-            let mockOrder = ZERO_ORDER
-            mockOrder.makeAsset.value = 100
-            mockOrder.takeAsset.value = 200
-            await expectThrow(
-                mockOrderLibrary.calculateRemainingValuesInOrder(mockOrder, 101, true)
-            );
-            await expectThrow(
-                mockOrderLibrary.calculateRemainingValuesInOrder(mockOrder, 201, false)
-            );
-        });
+            assert.equal(res[0], 1000)
+            assert.equal(res[1], 2000)
+        })
 
         it("should calculate remaining when fill isn't 0", async () => {
             let mockOrder = ZERO_ORDER
-            mockOrder.makeAsset.value = 100
-            mockOrder.takeAsset.value = 200
+            mockOrder.makeAsset.value = 1000
+            mockOrder.takeAsset.value = 2000
             let res = await mockOrderLibrary.calculateRemainingValuesInOrder(mockOrder, 5, true)
-            assert.equal(res[0], 100 - 5)
-            assert.equal(res[1], 200 / 100 * (100 - 5))
+            assert.equal(res[0], 1000 - 5)
+            assert.equal(res[1], 2000 / 1000 * (1000 - 5))
 
             res = await mockOrderLibrary.calculateRemainingValuesInOrder(mockOrder, 50, false)
-            assert.equal(res[0], 100 / 200 * (200 - 50))
-            assert.equal(res[1], 200 - 50)
-        });
+            assert.equal(res[0], 1000 / 2000 * (2000 - 50))
+            assert.equal(res[1], 2000 - 50)
+        })
+
+        it("should revert when fill > asset's values", async () => {
+            let mockOrder = ZERO_ORDER
+            mockOrder.makeAsset.value = 1000
+            mockOrder.takeAsset.value = 2000
+            await expectThrow(
+                mockOrderLibrary.calculateRemainingValuesInOrder(mockOrder, 1001, true)
+            )
+            await expectThrow(
+                mockOrderLibrary.calculateRemainingValuesInOrder(mockOrder, 2001, false)
+            )
+        })
+
+        it("should return 0 if it's a full fill", async () => {
+            let mockOrder = ZERO_ORDER
+            mockOrder.makeAsset.value = 1000
+            mockOrder.takeAsset.value = 2000
+
+            let res = await mockOrderLibrary.calculateRemainingValuesInOrder(mockOrder, 1000, true)
+            assert.equal(res[0], 0)
+            assert.equal(res[1], 0)
+
+            res = await mockOrderLibrary.calculateRemainingValuesInOrder(mockOrder, 2000, false)
+            assert.equal(res[0], 0)
+            assert.equal(res[1], 0)
+        })
+    })
+
+    describe("test OrderLibrary.checkTimeValidity()", () => {
+        it("should pass if zero timestamp is set", async () => {
+            let mockOrder = ZERO_ORDER
+            const now = getCurrentTimestamp()
+            // double zero timestamp
+            await mockOrderLibrary.checkTimeValidity(mockOrder)
+
+            // only zero timestamp
+            mockOrder.startTime = now - 1000
+            await mockOrderLibrary.checkTimeValidity(mockOrder)
+
+            mockOrder.startTime = 0
+            mockOrder.endTime = now + 1000
+            await mockOrderLibrary.checkTimeValidity(mockOrder)
+        })
+
+        it("should pass if two timestamps are correctly set", async () => {
+            let mockOrder = ZERO_ORDER
+            const now = getCurrentTimestamp()
+            mockOrder.startTime = now - 1000
+            mockOrder.endTime = now + 1000
+            await mockOrderLibrary.checkTimeValidity(mockOrder)
+        })
+
+        it("should revert if start timestamp is incorrectly set", async () => {
+            let mockOrder = ZERO_ORDER
+            const now = getCurrentTimestamp()
+            mockOrder.startTime = now + 100
+            mockOrder.endTime = now + 1000
+            await expectThrow(
+                mockOrderLibrary.checkTimeValidity(mockOrder),
+                "Order start validation failed"
+            )
+        })
+
+        it("should revert if end timestamp is incorrectly set", async () => {
+            let mockOrder = ZERO_ORDER
+            const now = getCurrentTimestamp()
+            mockOrder.startTime = now - 1000
+            mockOrder.endTime = now - 100
+            await expectThrow(
+                mockOrderLibrary.checkTimeValidity(mockOrder),
+                "Order end validation failed"
+            )
+        })
+
+        it("should revert if both timestamps are incorrectly set", async () => {
+            let mockOrder = ZERO_ORDER
+            const now = getCurrentTimestamp()
+            mockOrder.startTime = now + 1
+            mockOrder.endTime = now
+            await expectThrow(
+                mockOrderLibrary.checkTimeValidity(mockOrder),
+                "Order start validation failed"
+            )
+        })
+
     })
 });
+
+function getCurrentTimestamp() {
+    return parseInt(new Date() / 1000);
+}
