e3e97d7e3bce9bdef36bbf1680319a08ea6a9a63
MichaelWang <michael_wangguanyi@163.com>
2022-04-26 12:01:01 +0800

    fit ut for Cashier.sol

--- test/test_files/Cashier-test.js
+++ test/test_files/Cashier-test.js
@@ -63,11 +63,11 @@ contract("test Cashier.sol", accounts => {
         await mockERC721.mint(accounts[1], 1)
         await mockERC721.approve(mockNFTTransferProxy.address, 1, {from: accounts[1]})
         await expectThrow(
-            mockCashier.transfer(order.Asset(ERC721_CLASS, encode(mockERC721.address, 1), 2), accounts[1], accounts[2]),
+            mockCashier.transfer(order.Asset(ERC721_CLASS, encode(mockERC721.address, 1, []), 2), accounts[1], accounts[2]),
             "ERC721's strict amount"
         )
 
-        await mockCashier.transfer(order.Asset(ERC721_CLASS, encode(mockERC721.address, 1), 1), accounts[1], accounts[2])
+        await mockCashier.transfer(order.Asset(ERC721_CLASS, encode(mockERC721.address, 1, []), 1), accounts[1], accounts[2])
         assert.equal(await mockERC721.ownerOf(1), accounts[2])
         assert.equal(await mockERC721.balanceOf(accounts[1]), 0)
     })
@@ -76,12 +76,12 @@ contract("test Cashier.sol", accounts => {
         await mockBadERC721.mint(accounts[1], 1)
         await mockBadERC721.approve(mockNFTTransferProxy.address, 1, {from: accounts[1]})
         await expectThrow(
-            mockCashier.transfer(order.Asset(ERC721_CLASS, encode(mockBadERC721.address, 1), 2), accounts[1], accounts[2]),
+            mockCashier.transfer(order.Asset(ERC721_CLASS, encode(mockBadERC721.address, 1, []), 2), accounts[1], accounts[2]),
             "ERC721's strict amount"
         )
 
         await expectThrow(
-            mockCashier.transfer(order.Asset(ERC721_CLASS, encode(mockBadERC721.address, 1), 1), accounts[1], accounts[2]),
+            mockCashier.transfer(order.Asset(ERC721_CLASS, encode(mockBadERC721.address, 1, []), 1), accounts[1], accounts[2]),
             "bad safeTransferFrom of ERC721"
         )
 
@@ -92,7 +92,7 @@ contract("test Cashier.sol", accounts => {
     it("should transfer ERC1155 successfully", async () => {
         await mockERC1155.mint(accounts[1], 1, 1024)
         await mockERC1155.setApprovalForAll(mockNFTTransferProxy.address, true, {from: accounts[1]})
-        await mockCashier.transfer(order.Asset(ERC1155_CLASS, encode(mockERC1155.address, 1), 1000), accounts[1], accounts[2])
+        await mockCashier.transfer(order.Asset(ERC1155_CLASS, encode(mockERC1155.address, 1, []), 1000), accounts[1], accounts[2])
         assert.equal(await mockERC1155.balanceOf(accounts[1], 1), 24)
         assert.equal(await mockERC1155.balanceOf(accounts[2], 1), 1000)
     })
