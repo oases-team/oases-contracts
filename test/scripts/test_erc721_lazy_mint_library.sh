c733219c68ff4ad36ffce6b4aebfdd4a3289bf5f
MichaelWang <michael_wangguanyi@163.com>
2022-06-16 15:01:43 +0800

    change SignatureCheckerUpgradeable.isValidSignatureNow from openzeppelin

--- test/scripts/test_erc721_lazy_mint_library.sh
+++ test/scripts/test_erc721_lazy_mint_library.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 truffle test ./test/test_files/ERC721LazyMintLibrary.test.js \
-                ./test/contracts/MockERC721LazyMintLibrary.sol
+              ./test/contracts/MockERC721LazyMintLibrary.sol \
+              --compile-all
